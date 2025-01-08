74ea2f2a4d0542bd4a013a9c49a64fdc46395d72
Richard Gagg <r.gagg@outlook.com>
2025-01-08 13:12:48 +1100

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Richard Gagg on 8/1/2025.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
