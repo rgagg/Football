33aaaf7608325c5b136dbccf049ce1f6a006cbca
Richard Gagg <r.gagg@outlook.com>
2025-01-08 14:08:11 +1100

    All text and images added

--- Football/ContentView.swift
+++ Football/ContentView.swift
@@ -8,17 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+  var body: some View {
+    VStack {
+      
+      Text("What is Football to You?")
+        .font(.largeTitle)
+        .fontWeight(.thin)
+        .foregroundStyle(.green)
+      
+      HStack {
+        
+        Image(systemName: "figure.american.football")
+          .resizable()
+          .scaledToFit()
+          .foregroundStyle(.blue)
+        
+        Image(systemName: "figure.australian.football")
+          .resizable()
+          .scaledToFit()
+          .foregroundStyle(.indigo)
+        
+        Image(systemName: "figure.indoor.soccer")
+          .resizable()
+          .scaledToFit()
+          .foregroundStyle(.purple)
+        
+      }
     }
+    .padding()
+  }
 }
 
 #Preview {
-    ContentView()
+  ContentView()
 }
